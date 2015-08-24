913422a029826d943884f79e7f02fbd78c97fa6e
ChaseTerzian <chaseterzian@gmail.com>
2015-08-23 19:44:11 -0600

    revisions, new links to SQ and all things in the projects section

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+	$('#demo-videos-link').on('click', function() {
+		alert('coming soon');
+	});
+
+	$('#download-resume-link').on('click', function() {
+		window.location = 'https://www.dropbox.com/s/kzpgo50phhhklmr/resume.pages?dl=0'
+	});
+
+	$('#github-link').on('click', function() {
+	window.location = 'https://github.com/chaseterzian?tab=repositories'
+	});
+
+});
