329ac6d7cf6c7016b4ff82bf307a1ef8e01b9219
ChaseTerzian <chaseterzian@gmail.com>
2015-12-03 15:01:00 -0700

    added links to PDF, pages, word, word causes unintentional load in main tab when force opening in a new tab

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,9 +4,16 @@ $(document).ready(function() {
 		alert('coming soon');
 	});
 
-	$('#download-resume-link').on('click', function() {
-		window.location = 'https://www.dropbox.com/s/2xntt28exg7o1wq/resume.pdf?dl=0'
+	$('#download-resume-link-pdf').on('click', function() {
+		window.location = 'https://www.dropbox.com/s/y0zeihrbkr97wtz/Resume.pdf?dl=0'
 	});
+	$('#download-resume-link-pages').on('click', function() {
+		window.location = 'https://www.dropbox.com/s/omphifs9wj2msff/resume.pages?dl=0'
+	});
+	$('#download-resume-link-word').on('click', function() {
+		window.location = 'https://www.dropbox.com/s/estgz6yipcf562m/ResumeWord.docx?dl=0'
+	});
+
 
 	$('#github-link').on('click', function() {
 	window.location = 'https://github.com/chaseterzian?tab=repositories'
