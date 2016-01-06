81a2f0ee2a622499875786b73cc81fb03c790de3
ChaseTerzian <chaseterzian@gmail.com>
2016-01-05 21:35:04 -0700

    content rework to match new res

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -7,12 +7,12 @@ $(document).ready(function() {
 	$('#download-resume-link-pdf').on('click', function() {
 		window.location = 'https://www.dropbox.com/s/y0zeihrbkr97wtz/Resume.pdf?dl=0'
 	});
-	$('#download-resume-link-pages').on('click', function() {
-		window.location = 'https://www.dropbox.com/s/omphifs9wj2msff/resume.pages?dl=0'
-	});
-	$('#download-resume-link-word').on('click', function() {
-		window.location = 'https://www.dropbox.com/s/estgz6yipcf562m/ResumeWord.docx?dl=0'
-	});
+	// $('#download-resume-link-pages').on('click', function() {
+	// 	window.location = 'https://www.dropbox.com/s/omphifs9wj2msff/resume.pages?dl=0'
+	// });
+	// $('#download-resume-link-word').on('click', function() {
+	// 	window.location = 'https://www.dropbox.com/s/estgz6yipcf562m/ResumeWord.docx?dl=0'
+	// });
 
 
 	$('#github-link').on('click', function() {
